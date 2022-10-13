383d5ff3a711dc7c23f9d2705b6f187799712f47
Dominik Ziolkowski <minickisbeast@gmail.com>
2022-10-13 08:36:09 -0400

    Added more to the main menu
    Fixed up some stuff from the main menu

--- Assets/MyGame/Scripts/Managers/MainMenuManager.cs
+++ Assets/MyGame/Scripts/Managers/MainMenuManager.cs
@@ -1,9 +1,43 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenuManager : MonoBehaviour
 {
+    public void LoadMainGame(){
+    
+    }
+
+
+    public void LoadConceptArt(){
+    
+    }
+    
+    public void ExitGame(){
+    
+    }
+    
+    
+    public IEnumerator RealSceneLoadAsynchronousLoad(string sceneToLoad)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(sceneToLoad);
+        ao.allowSceneActivation = false;
+
+        while (!ao.isDone)
+        {
+            float progress = Mathf.Clamp01(ao.progress / 0.9f);
+           // LoadingText.text = "Loading: " + (int)(progress * 100) + "%";
+
+            if (ao.progress == 0.9f)
+                ao.allowSceneActivation = true;
+
+            yield return null;
+        }
+    }
+    
+
+
     // Start is called before the first frame update
     void Start()
     {
